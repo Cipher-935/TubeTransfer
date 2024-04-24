d6d10b1481fdb863fad5e768ebbbe567476e5648
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2024-04-24 11:55:52 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const bodyParser = require('body-parser');
+const route_handler = require("./Routes/app_routes.js");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json()); // Middleware to exchange data in json format
+
+// Serve static files from the 'templates' directory
+app.use('/templates', express.static(path.join(__dirname, 'templates'))); // For serving files that are static
+
+app.use("/", route_handler);
+
+app.get("*", (req,res) => {
+    res.status(200).json({
+        resp: 'Not supported'
+    });
+});
+server.listen(4000, () =>  {
+   console.log("App has stared");
+});
+
+
+
+

c507f7799e14968fb6c90a9715c4b9ab284c9cb9
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2024-04-26 15:53:37 -0700

    new back

--- backend/controllers/app_controller.js
+++ backend/controllers/app_controller.js
@@ -3,8 +3,11 @@ const path = require("path");
 const { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand, ListObjectsV2Command } = require("@aws-sdk/client-s3");
 const {getSignedUrl} = require("@aws-sdk/s3-request-presigner");
 const file_model =  require("../models/file_information_model.js");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const error_h = require("../middlewares/error_class.js");
 
+
+// Make the S3 client globally so all functions can access it so as to be modular
 const s3 = new S3Client({
     credentials:{
         accessKeyId: process.env.ACCESS_KEY,
@@ -13,8 +16,11 @@ const s3 = new S3Client({
     region: process.env.bucket_location
 });
 
-exports.get_home = async (req, res) => {
-    try{
+// Make the storage location path for the s3 bucket as per the file etension and than return, makes putObject method modular
+
+
+// Return the static template for the home.html from the templates directory
+exports.get_home = async (req, res, next) => {
         let file_path = path.join(__dirname, "../templates/home.html");
       
         if(fs.existsSync(file_path)){
@@ -22,113 +28,89 @@ exports.get_home = async (req, res) => {
             res.sendFile(file_path);
         }
         else{
-            res.status(404).json({
-                resp: "File not found"
-            });
+            return next(new error_h("Requested file is not found", 404));
         }
-    }
-    catch(e){
-        res.status(404).json({
-            resp: "Some error on the server side"
-        });
-    }
+       
 };
 
 // This functon returns a presigned url to access a private file on the s3.
-exports.get_object = async (req,res) => {
-    try{
-        const {get_key} = req.body;
-        const param = {
-            Bucket: process.env.bucket_name,
-            Key: get_key
-        };
-        const command = new GetObjectCommand(param);
-    
-        const url = await getSignedUrl(s3, command, {expiresIn: 60*2, signingDate: new Date()});
-        console.log(url);
+exports.get_object = async (req,res,next) => {
+    const {get_key} = req.body;
+    const param = {
+        Bucket: process.env.bucket_name,
+        Key: get_key
+    };
+    const command = new GetObjectCommand(param);
+    const url = await getSignedUrl(s3, command, {expiresIn: 60*2, signingDate: new Date()});
+    if(url){
         res.status(200).json({
             resp: url
         });
     }
-    catch(e){
-        console.log(e);
-         res.status(404).json({
-            resp: "Some error has occured on server side"
-         });
-    }
+    else{
+      return next(new error_h("Couldn't get the file at the moment, check the name again", 500));
+    } 
 }
 
 // This function is used to delete the object form the s3
-exports.delete_object = async (req,res) => {
+exports.delete_object = async (req,res,next) => {
     const {main_key} = req.body;
      const param = {
          Bucket: process.env.bucket_name,
          Key: main_key
      };
-     
      const command = new DeleteObjectCommand(param);
-     try{
-        const del_response = await s3.send(command);
+     const del_response = await s3.send(command);
+     if(del_response){
         res.status(200).json({
             resp: "Deleted"
-        })
-     }
-     catch(e){
-        res.status(404).json({
-            resp: "Could not delete"
         });
+     }
+     else{
+        return next(new error_h("File was not deleted, check the key again", 500));
      }
 }
 
-exports.list_objects = async (req,res) => {
+// This function will list all the files present in the s3 bucket
+exports.list_objects = async (req,res,next) => {
     const list_command = new ListObjectsV2Command({
         Bucket: process.env.bucket_name
     });
     const result = await s3.send(list_command);
-    res.status(200).json({
-        resp: result.Contents
-    });
-
+    if(result){
+        res.status(200).json({
+            resp: result.Contents
+        });
+    }
+    else{
+        return next(new error_h("File was not deleted, check the key again", 500));
+    }
 }
 
-exports.put_object_url = async (req,res) => {
+// This function returns the signed url for the put request method giving access toprivate s3
+exports.put_object_url = async (req,res,next) => {
+    const {file_name, file_type, file_description, file_mime} = req.body;
+    let storage_path = path_builder(file_name);
     try{
-        const {file_name, file_type, file_description, file_mime} = req.body;
-        const file_extension = file_name.split(".").pop();
-        let loc = '';
-        if(file_extension === "txt"){
-            loc += "documents/" + file_name;
-        }
-        else if(file_extension === 'mp4'){
-            loc += "videos/" + file_name;
-        }
-        else{
-            loc += file_name;
-        }
-        const file_add = await file_model.create({uploded_file_name: file_name, uploaded_file_extension: file_extension, uploaded_file_description: file_description, uploaded_file_type: file_type, uploaded_file_storage_location: loc});
-        if(file_add){
-            const put_command = new PutObjectCommand({
-                Bucket: process.env.bucket_name,
-                Key: loc,
-                ContentType: file_mime
-            });
-            const put_url = await getSignedUrl(s3, put_command, {expiresIn: 6000});
-            res.status(200).json({
-                resp: put_url
-            });
-        }
-        else{
-            res.status(404).json({
-                resp: "Could not save the data to db"
-           });  
-        }
+        const file_add = await file_model.create({uploded_file_name: file_name, uploaded_file_description: file_description, uploaded_file_type: file_type, uploaded_file_storage_location: storage_path});
     }
     catch(e){
-       res.status(404).json({
-            resp: "Error saving the data: " + e
-       });  
+        return next(new error_h("Could not add file details to the schema, check the post data", 500));
     }
-
+    try{
+        const put_command = new PutObjectCommand({
+            Bucket: process.env.bucket_name,
+            Key: res.locals.store_path,
+            ContentType: res.locals.ftype
+        });
+        const put_url = await getSignedUrl(s3, put_command, {expiresIn: 6000});
+        res.status(200).json({
+            resp: put_url
+        });
+    }
+    catch(e){
+         return next(new error_h("Url was not generated due to some error", 500));
+    }
 }
 
 
---
+++ backend/middlewares/schema_save.js
@@ -0,0 +1,36 @@
+const error_h = require("../middlewares/error_class.js");
+
+const path_builder = function(file_name){
+    const extension = file_name.split(".").pop();
+    let loc = '';
+    if(extension === "txt" || extension === "docx" || extension === "pdf" || extension === "pptx" || extension === "rtf"){
+        loc += "documents/" + file_name;
+        return loc;
+    }
+    else if(extension === "mp4" || extension === "mov" || extension === "mkv" || extension === "avi"){
+        loc += "videos/" + file_name;
+        return loc;
+    }
+    else if(extension === "png" || extension === "jpeg" || extension === "jpg" || extension === "bmp" || extension === "ico"){
+        loc += "images/" + file_name;
+        return loc;
+    }
+    else{
+        loc += "other/" + file_name;
+        return loc;
+    }
+}
+
+exports.save_file_data = async (req,res,next) => {
+    const {file_name, file_type, file_description, file_mime} = req.body;
+    let storage_path = path_builder(file_name);
+    try{
+        const file_add = await file_model.create({uploded_file_name: file_name, uploaded_file_description: file_description, uploaded_file_type: file_type, uploaded_file_storage_location: storage_path});
+        res.locals.store_path = storage_path;
+        res.locals.ftype = file_mime;
+        next();
+    }
+    catch(e){
+        return next(new error_h("Could not add file details to the schema, check the post data", 500));
+    }
+}
